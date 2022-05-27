eb48b06e41c4e39be35687e24e23bc23128ae97e
Jose Luis Carrasco <92541048+JoseLuCD@users.noreply.github.com>
2022-05-27 20:37:56 +0200

    Examen Entornos V2

--- Examen_Entornos/Examen_Entornos/Asignatura.cs
+++ Examen_Entornos/Examen_Entornos/Asignatura.cs
@@ -3,126 +3,123 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace Examen_Entornos
-{
-    
-
-
+{    
     class Asignatura
     {
-        public Asignatura(string CodigodelaAsignatura, string NombredelaAsignatura, double NotadelPrimertrimestre, double NotadelSegungoTrimestre, double NotadelTercerTrimestre, double NotaFinaldelCurso)
+        public string CodigoAsignatura;
+        public string NombreAsignatura;
+        public double NotaPrimerTrimestre;
+        public double NotaSegungoTrimestre;
+        public double NotaTercerTrimestre;
+        public double NotaFinal;
+        //Aqui hemos cambiado el nombre y la guia de estilos de estos para que sean mas legibles y hemos cambiado su posicion arriba ya que por estilo las variables suelen ir el la parte de arriba
+
+        public Asignatura(string CodigoAsignatura, string NombreAsignatura, double NotaPrimerTrimestre, double NotaSegungoTrimestre, double NotaTercerTrimestre, double NotaFinal)
         {
-            this.CodigodelaAsignatura = CodigodelaAsignatura; //Codigo de la asignatura.
-            this.NombredelaAsignatura = NombredelaAsignatura; //Nombre de la asignatura.
-                                                              // Nota de los trimestres (1, 2   y 3) y nota final.
-            this.NotadelPrimertrimestre = NotadelPrimertrimestre;
-            this.NotadelSegungoTrimestre = NotadelSegungoTrimestre;
-            this.NotadelTercerTrimestre = NotadelTercerTrimestre;
-            this.NotaFinaldelCurso = NotaFinaldelCurso;
+            this.CodigoAsignatura = CodigoAsignatura;
+            this.NombreAsignatura = NombreAsignatura;
+            this.NotaPrimerTrimestre = NotaPrimerTrimestre;
+            this.NotaSegungoTrimestre = NotaSegungoTrimestre;
+            this.NotaTercerTrimestre = NotaTercerTrimestre;
+            this.NotaFinal = NotaFinal;
+            //En este codigo, lo que hemos hecho ha sido la tabulacion para quitar espacios en blanco
         }
 
 
-
-        public string CodigodelaAsignatura;
-        public string NombredelaAsignatura;
-        public double NotadelPrimertrimestre;
-        public double NotadelSegungoTrimestre;
-        public double NotadelTercerTrimestre;
-        public double NotaFinaldelCurso;
-
-
-
-        public void IntroducirMateria(string Cod_de_Asignatura, string Nombre_de_Asignatura)
+        public void IntroducirMateria(string codAsignatura, string nombreAsignatura)
         {
-            this.CodigodelaAsignatura = Cod_de_Asignatura; this.NombredelaAsignatura = Nombre_de_Asignatura;
-
+            this.CodigoAsignatura = codAsignatura; this.NombreAsignatura = nombreAsignatura;
+            //En este codigo hemos hecho cambios en las variables para que sean mas legibles y hemos quitado espacios en blanco.
         }
 
-        public void IntroducirlasNotas(double notadelExamen1, double notadelExamen2, double notadelExamen3, double notadelasPracticas1, double notadelasPracticas2, double notadelasPracticas3)
+        /// <summary>
+        /// En esta funcion hemos hecho varias refactorizaciones como la buena tabulacion del codigo, quitar llaves innecesarias y reordenar los campos para que el codigo tenga sentido.
+        /// Tambien hemos hecho unos cambios en el codigo para que las variables no se queden sin hacer nada.
+        /// </summary>
+        /// <param name="notaExamen1"></param>
+        /// <param name="notaExamen2"></param>
+        /// <param name="notaExamen3"></param>
+        /// <param name="notaPracticas1"></param>
+        /// <param name="notaPracticas2"></param>
+        /// <param name="notaPracticas3"></param>
+        /// <returns>Esta funcion imoprimira en pantalla la nota de las practicas y examenes</returns>
+        public void IntroducirlasNotas(double notaExamen1, double notaExamen2, double notaExamen3, double notaPracticas1, double notaPracticas2, double notaPracticas3)
         {
-
+            for (int i = 1; i < 4; i++)
             {
-                for (int i = 1; i < 4; i++)
+                Console.WriteLine("Trimestre evaluado 1, 2 ó 3: ");
+                var Trimestre = Console.ReadLine();
+
+                switch (Trimestre)
                 {
-                    Console.WriteLine("Trimestre evaluado 1, 2 ó 3: ");
-                    var Trimestre = Console.ReadLine();
+                    case "1":
+                        notaPracticas1 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Nota de las prácticas: ", notaPracticas1);
+                        notaExamen1 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Nota del examen: ", notaExamen1);
+                        break;
 
+                    case "2":
+                        notaPracticas2 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Nota de las prácticas: ", notaPracticas2);
+                        notaExamen2 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Nota del examen: ", notaExamen2);
+                        break;
 
-
-
-                    switch (Trimestre)
-                    {
-                        case "1":
-                            Console.WriteLine("Nota de las prácticas: ");
-                            notadelasPracticas1 = Convert.ToDouble(Console.ReadLine());
-                            Console.WriteLine("Nota del examen: ");
-                            notadelExamen1 = Convert.ToDouble(Console.ReadLine());
-                            break;
-
-                        case "2":
-                            Console.WriteLine("Nota de las prácticas: ");
-                            notadelasPracticas2 = Convert.ToDouble(Console.ReadLine());
-                            Console.WriteLine("Nota del examen: ");
-                            notadelExamen2 = Convert.ToDouble(Console.ReadLine());
-                            break;
-
-                        case "3":
-                            Console.WriteLine("Nota de las prácticas: ");
-                            notadelasPracticas3 = Convert.ToDouble(Console.ReadLine());
-                            Console.WriteLine("Nota del examen: ");
-                            notadelExamen3 = Convert.ToDouble(Console.ReadLine());
-                            break;
-                    }
-
-
+                    case "3":
+                        notaPracticas3 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Nota de las prácticas: ", notaPracticas3);
+                        notaExamen3 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Nota del examen: ", notaExamen3);
+                        break;
                 }
-
             }
         }
 
-
-
-
-
-
-
-
-
-        public double calculodelaNota1(double notadelExamen1, double notadelasPracticas1)
+        /// <summary>
+        /// Para estas cuatro funciones hemos encapsulado el valor final de la nota en una variable para que sea todo mas entendible. Ademas hemos cambiado sus nombres para hacerlo mas legible
+        /// </summary>
+        /// <var name="nota">Esta variable dice la nota de cada trimestre</var>
+        /// <returns>devuelve la nota del primer trimestre</returns>
+        public double calculodelaNota1(double notaExamen1, double notaPracticas1)
         {
-            this.NotadelPrimertrimestre = (notadelExamen1 * 0.8) + (notadelasPracticas1) * 0.2; //La nota está comupuesta  por 20 % de la nota de prácticas y 80 % de la nota del examen. 
-            return this.NotadelPrimertrimestre;
+            var nota = (notaExamen1 * 0.8) + (notaPracticas1 * 0.2);
+            this.NotaPrimerTrimestre = nota;
+            return this.NotaPrimerTrimestre;
         }
-        public double calculodelaNota2(double notadelExamen2, double notadelasPracticas2)
+
+        /// <summary>
+        /// </summary>
+        /// <param name="notaExamen2"></param>
+        /// <param name="notaPracticas2"></param>
+        /// <returns>devuelve la nota del segundo trimestre</returns>
+        public double calculodelaNota2(double notaExamen2, double notaPracticas2)
         {
-            this.NotadelSegungoTrimestre = (notadelExamen2 * 0.8) + (notadelasPracticas2) * 0.2;
-            return this.NotadelSegungoTrimestre;
+            var nota = (notaExamen2 * 0.8) + (notaPracticas2 * 0.2);
+            this.NotaSegungoTrimestre = nota;
+            return this.NotaSegungoTrimestre;
         }
-        public double calculodelaNota3(double notadelExamen3, double notadelasPracticas3)
+
+        /// <summary>
+        /// </summary>
+        /// <param name="notaExamen3"></param>
+        /// <param name="notaPracticas3"></param>
+        /// <returns>devuelve la nota del tercer trimestre</returns>
+        public double calculodelaNota3(double notaExamen3, double notaPracticas3)
         {
-            this.NotadelTercerTrimestre = (notadelExamen3 * 0.8) + (notadelasPracticas3) * 0.2;
-            return this.NotadelTercerTrimestre;
+            var nota = (notaExamen3 * 0.8) + (notaPracticas3 * 0.2);
+            this.NotaTercerTrimestre = nota;
+            return this.NotaTercerTrimestre;
         }
+
+        /// <summary>
+        /// </summary>
+        /// <returns>Devuelvela nota media</returns>
         public double Calculodelamedia()
         {
-            this.NotaFinaldelCurso = (this.NotadelPrimertrimestre + this.NotadelSegungoTrimestre + this.NotadelTercerTrimestre) / 3; //Nota final del curso
-            return this.NotaFinaldelCurso;
+            var media = (this.NotaPrimerTrimestre + this.NotaSegungoTrimestre + this.NotaTercerTrimestre) / 3;
+            this.NotaFinal = media;
+            return this.NotaFinal;
         }
-
-    }
-    class ejemplodeunaAsignatura
-    {
-
-        //TODO
-        /*
-         *   
-        static void main()
-        {
-            * 
-            * 
-            * 
-            asignatura mi_asignatura_ejemplo = new asignatura("0521385", "Entornos  de Desarrollo");
-
-            /
-        */
     }
 }
---
+++ Examen_Entornos/Examen_Entornos/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Examen_Entornos
+{
+    class Program
+    {
+        /// <summary>
+        /// Esta clase la hemos extraido del codigo anterior para meterla en su propia clase ya que es un main.
+        /// En este main simplemente se crea una asignatura y se le asignan parametros.
+        /// </summary>
+        static void main()
+        {
+            Asignatura asigejemplo = new Asignatura("0521385", "Entornos  de Desarrollo",5,5,5,5);
+        }
+    }
+}
